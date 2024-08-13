c817f27c73072c787a0b8f942e2abba56f95d662
Sumit Khandelwal <synergy2411@outlook.com>
2024-08-13 10:12:10 +0530

    initial commit

---
+++ src/main/java/com/example/github_actions/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.github_actions.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DemoController {
+
+    @GetMapping("/greet")
+    public String greet(){
+        return "Hello World!";
+    }
+
+}
